9bf360c49aadd5b4fbea912c6ea8328b90a8273b
Tân Đậu <69078888+tandaujava@users.noreply.github.com>
2020-11-12 02:59:46 +0700

    ...

--- src/Buoi9/Main_B5.java
+++
@@ -1,9 +0,0 @@
-package Buoi9;
-
-public class Main_B5 {
-    public static void main(String[] args) {
-        Class n1 = new Class();
-        n1.inputStudent();
-        n1.outputStudent();
-    }
-}
